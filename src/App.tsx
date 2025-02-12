3280894f43eba3e34766fbcac2a024f900b7aff9
K.Kazuyuki <{kisidakazuyuki@gmail.com}>
2025-02-12 17:14:36 +0900

    依存関係の追加とアプリの初期コンポーネントの更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
+import { Typography } from '@mui/material'
 
 function App() {
   return (
-    <>    </>
+    <>
+        <Typography variant="h1" color="textPrimary">Welcome to jimin.party</Typography>
+        <Typography variant="h2" color="secondary">Just In MINutes Party.</Typography>
+    </>
   )
 }
 
